cf99e1dd2d542e0da42fdc7653c54b7c3413022e
wassimxd <karimmohsin10@gmail.com>
2024-04-22 16:58:38 +0100

    initial commit

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,71 @@
+import { Input } from "./Input";
+import { useRef } from "react";
+import Modal from "./Modal";
+
+export const NewProject = ({ onAdd, onCancel }) => {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  const modal = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
+  return (
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="my-4 text-xl text-stone-700 font-bold">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Ooops... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              onClick={onCancel}
+              className="text-stone-800 hover:text-stone-950"
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,25 @@
+import noProjectImage from "../assets/no-projects.png";
+import Button from "./Button";
+
+const NoProjectSelected = ({ onStartAddProject }) => {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjectImage}
+        alt="An empty task list"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="my-4 text-xl text-stone-500 font-bold">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with a new one.
+      </p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>Create a new project</Button>
+      </p>
+    </div>
+  );
+};
+
+export default NoProjectSelected;
